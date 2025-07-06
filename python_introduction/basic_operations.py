d31c1a57c0b6a21a74143c0acae28f4169dde785
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-07-06 16:48:09 -0700

    Add basic_operations.py for arithmetic practice

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+addition= number1 + number2
+substraction= number1 - number2
+multiplication= number1 * number2
+
+print('Addition of', number1, 'and', number2, 'is', addition)
+print('Substraction of', number1, 'and', number2, 'is', substraction)
+print('Multiplication of', number1, 'and', number2, 'is', multiplication)
+
