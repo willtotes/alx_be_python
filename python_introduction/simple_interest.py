3a13ee457ad913e8a61a424715d317468be8762d
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-07-07 06:59:10 -0700

    Add simple_interest.py for application of arithmetic operations and variable assignments to calculate simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,9 @@
-principle= 1000
-rate= 0.05
-time= 3
 
-interest= principle * rate * time
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
 
 print('The simple interest is:', interest)
 
