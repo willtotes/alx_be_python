a66d26cfa8222c117c2e76ae5b5c07918bf4d3e8
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-07-15 01:05:33 +0200

    Add pattern_drawing.py for drawing patterns using loops

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+
+#print("*", end="")
+pattern = int(input("Enter the size of the pattern"))
+
+for pattern in range(1, 10):
+    for i in range(1, 10):
+        print("*", end="")
+    print()
+
+    #edit
