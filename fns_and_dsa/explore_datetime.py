8193860f588523579fcbf43ec74229074abbf803
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-07-25 12:22:02 +0200

    Update explore_datetime.py file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
 
 def calculate_future_date():
     try:
-        days_to_add = int(input("Enter number of days to add: "))
+        days_to_add = int(input("Enter the number of days to add to the current date: "))
         future_date = datetime.now() + timedelta(days = days_to_add)
         print(f"Future Date: {future_date.strftime('%Y-%m-%d')}")
     except ValueError:
@@ -16,6 +16,6 @@ def calculate_future_date():
 if __name__ == "__main__":
     display_current_datetime()
     calculate_future_date()
-    
+
 
 
