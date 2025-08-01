44e2ba88a8a7fcd0b697cdf2f9a968e6ffa25760
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-08-01 18:47:44 +0200

    Increased the current balance from 00 to 50

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -2,7 +2,7 @@ import sys
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount(100)
+    account = BankAccount(250)
     if len(sys.argv) < 2:
         print("Usage: python main-0.py <command>:<amount>")
         print("Commands deposit, withdraw, display")
