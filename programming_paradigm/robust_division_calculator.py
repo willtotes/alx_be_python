4137c29b31bbc71301bde75e237204ed51d49cb6
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-08-02 00:37:33 +0200

    Reorganising files

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -14,7 +14,7 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero"
     except ValueError:
-        return "Error: Please enter numeric values"
+        return "Error: Please enter numeric values only."
     
 
 
