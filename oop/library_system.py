0a9751a6819cbe1ede77e01541ee427297eae6be
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-08-09 00:14:25 +0200

    Fixed output spelling error

--- oop/library_system.py
+++ oop/library_system.py
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.title}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
